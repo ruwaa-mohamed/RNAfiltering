8f6b44cd3b52aa670dae422a328842eba6f6db17
Ruwaa-Ib <ruwaa.ibrahem@gmail.com>
2019-04-28 18:40:00 +0200

    modefied main

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 from Bio.SeqUtils import GC
+from matplotlib import pyplot as plt
 
-def get_stats(reads):
+def graphing(GC_cont, avg_qual, state):
+    plt.rcParams.update({'font.size': 24})
+    plt.figure(1)
+    plt.hist(avg_qual)
+    plt.xlabel("Average Quality Score per Read")
+    plt.ylabel("Number of Reads")
+    plt.title(state + " Reads Averag Quality Score")
+
+    plt.figure(2)
+    plt.hist(GC_cont)
+    plt.xlabel("Average GC content per Read")
+    plt.ylabel("Number of Reads")
+    plt.title(state+ " Reads Averag GC Content")
+    plt.show()
+
+
+def get_stats(reads, state):
     sizes = []
     GC_cont = []
     avg_qual = []
@@ -20,9 +37,8 @@ def get_stats(reads):
     print("Min. read length: %i" % min(sizes))
     print("GC content: %i" % statistics.mean(GC_cont))
     print()
+    graphing(GC_cont, avg_qual, state)
 
-def graphing():
-    pass
 
 def quality_filter(reads, qual=20):
     return (r for r in reads if int(statistics.mean(r.letter_annotations["phred_quality"])) > qual)
@@ -48,8 +64,8 @@ def main(fq, adapt, avg_qual=20, min_length=15):
     # parse the fastq file
     reads = SeqIO.parse(fq, "fastq")
     # initial stats
-    print("Statistics of the raw reads:")
-    get_stats(reads)
+    print("Statistics of the Raw reads:")
+    get_stats(reads, "Raw")
     # graphing?
 
     # parse the file again
@@ -70,11 +86,12 @@ def main(fq, adapt, avg_qual=20, min_length=15):
     # final stats
     final_reads = SeqIO.parse(new_fq, "fastq")
     print("Statistics of the processed reads:")
-    get_stats(final_reads)
+    get_stats(final_reads, "Final")
     # graphing?
 
 ###Testing
 fq = 'SRR2079499_1.fastq'
 adapt = "AGGCCTGTCTCCTCTGAGTGATTGAC"
 ##main(fq, adapt)
-
+reads = SeqIO.parse(fq, "fastq")
+get_stats(reads, "Raw")
