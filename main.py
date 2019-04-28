9e2e12b15c15d3d8d152f9a9d60168e49541faf7
Ruwaa-Ib <ruwaa.ibrahem@gmail.com>
2019-04-28 18:01:57 +0200

    modefied main

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from Bio.Alphabet import IUPAC
 from Bio.SeqUtils import GC
 
 def get_stats(reads):
-    sizes = [len(r.seq) for r in reads]
-    GC_cont = [GC(r.seq) for r in reads]
+    sizes = []
+    GC_cont = []
+    avg_qual = []
+    for r in reads:
+        sizes.append(len(r.seq))
+        GC_cont.append(GC(r.seq))
+        avg_qual.append(statistics.mean(r.letter_annotations["phred_quality"]))
+##    sizes = [len(r.seq) for r in reads]
+##    GC_cont = [GC(r.seq) for r in reads]
     print("Total reads: %i" % len(sizes))
     print("Mean read length: %i" % statistics.mean(sizes))
     print("Max. read length: %i" % max(sizes))
     print("Min. read length: %i" % min(sizes))
-##    print(GC_cont)
-##    print("GC content: %i" % statistics.mean(GC_cont) )
+    print("GC content: %i" % statistics.mean(GC_cont))
     print()
 
 def graphing():
@@ -42,6 +48,7 @@ def main(fq, adapt, avg_qual=20, min_length=15):
     # parse the fastq file
     reads = SeqIO.parse(fq, "fastq")
     # initial stats
+    print("Statistics of the raw reads:")
     get_stats(reads)
     # graphing?
 
@@ -62,11 +69,12 @@ def main(fq, adapt, avg_qual=20, min_length=15):
     SeqIO.write(min_len_filter, new_fq , 'fastq')
     # final stats
     final_reads = SeqIO.parse(new_fq, "fastq")
+    print("Statistics of the processed reads:")
     get_stats(final_reads)
     # graphing?
 
 ###Testing
 fq = 'SRR2079499_1.fastq'
 adapt = "AGGCCTGTCTCCTCTGAGTGATTGAC"
-main(fq, adapt)
+##main(fq, adapt)
 
